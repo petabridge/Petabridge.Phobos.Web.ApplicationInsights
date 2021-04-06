5e126356eaccb6f0bb57c58feb52082ef5007559
Aaron Stannard <aaron@petabridge.com>
2021-04-06 14:44:50 -0500

    Initial commit

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.1.3 June 17 2020 ####
+First release
---
+++ src/Petabridge.Phobos.Web/Program.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2021 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Petabridge.Phobos.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureSerilogLogging()
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+    }
+}
