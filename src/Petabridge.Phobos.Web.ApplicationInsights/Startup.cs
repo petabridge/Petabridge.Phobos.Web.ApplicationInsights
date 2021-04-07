f0a7c3d08df3dfda6b7176d65859cb3776cb52df
Aaron Stannard <aaron@petabridge.com>
2021-04-07 16:44:50 -0500

    fixed bug with checking for Application Insights toggle

--- src/Petabridge.Phobos.Web.ApplicationInsights/Startup.cs
+++ src/Petabridge.Phobos.Web.ApplicationInsights/Startup.cs
@@ -36,7 +36,7 @@ namespace Petabridge.Phobos.Web.ApplicationInsights
         /// </summary>
         public const string AppInsightsInstrumentationKeyVariableName = "APP_INSIGHTS_INSTRUMENTATION_KEY";
 
-        public static bool IsAppInsightsEnabled => string.IsNullOrEmpty(
+        public static bool IsAppInsightsEnabled => !string.IsNullOrEmpty(
             Environment.GetEnvironmentVariable(AppInsightsInstrumentationKeyVariableName));
 
         // This method gets called by the runtime. Use this method to add services to the container.
